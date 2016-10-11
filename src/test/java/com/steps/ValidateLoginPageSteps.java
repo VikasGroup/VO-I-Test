c98df3127fe3eafdf6b3324ada0235a76d87cf7e
Pavithri <Pavithri@Pavithri-PC>
2016-10-11 17:25:10 +0530

    3rd commit

--- src/test/java/com/steps/ValidateLoginPageSteps.java
+++ src/test/java/com/steps/ValidateLoginPageSteps.java
@@ -1,17 +1,25 @@
 package com.steps;
 
-import cucumber.api.PendingException;
+import org.junit.Assert;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import com.voone.pgobjects.Online_Login_VOPgObject;
+import com.voone.pgobjects.loginPgObject;
+import com.voone.pgobjects.qntestPgObject;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class ValidateLoginPageSteps extends BasicTest {
 	
-	
+	loginPgObject loginpgObject;
+	qntestPgObject qntestpgObject;
+	Online_Login_VOPgObject online_Login_VOPgObject;
+
 
 	@Given("^User opens \"([^\"]*)\" home page$")
 	public void user_opens_home_page(String plan) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
 		System.out.println(prop.getProperty("url"));
 		driver.get(prop.getProperty("url"));
 	 
@@ -19,19 +27,77 @@ public class ValidateLoginPageSteps extends BasicTest {
 
 	@When("^User click on ir login$")
 	public void user_click_on_ir_login() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	   // throw new PendingException();
+		qntestpgObject = new qntestPgObject(driver);
+		qntestpgObject.clickIRLogiIn();
 	}
 
 	@When("^User enter valid \"([^\"]*)\" and \"([^\"]*)\"$")
-	public void user_enter_valid_and(String arg1, String arg2) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	public void user_enter_valid_and(String uname, String pwd) throws Throwable {
+		online_Login_VOPgObject = new Online_Login_VOPgObject(driver);
+		online_Login_VOPgObject.enterIRID(uname);
+		online_Login_VOPgObject.enterPassword(pwd);
+		Thread.sleep(2000);
+		online_Login_VOPgObject.clickLogInBtn();
+		
 	}
 
 	@Then("^User should able to login to the system$")
 	public void user_should_able_to_login_to_the_system() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	 Assert.assertEquals(driver.getCurrentUrl(), "https://portal.qntest.com/VirtualOffice/OnlineEStore/PopupsLoad.aspx");
 	}
+	
+	@When("^click on vo_option \"([^\"]*)\"$")
+	public void click_on_vo_option(String arg1) throws Throwable {
+		 loginpgObject = new loginPgObject(driver);
+		 Thread.sleep(9000);
+		  if (arg1.equals("current")) {
+			  loginpgObject.clickCurrentVirtualOffice();
+			 Thread.sleep(1000);
+		} 
+	}
+
+	@When("^select the language \"([^\"]*)\"$")
+	public void select_the_language(String language) throws Throwable {
+	online_Login_VOPgObject = new Online_Login_VOPgObject(driver);
+	online_Login_VOPgObject.selectLanguage(language);
+	Thread.sleep(1000);
+		
+	}
+	
+	@Then("^User should not be able to login to the system$")
+	public void user_should_not_be_able_to_login_to_the_system() throws Throwable {
+		try{
+			  
+			   WebDriverWait wait = new WebDriverWait(driver, 4);
+			   Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+
+			   //Accepting alert.
+			   alert.accept();
+			  
+			}catch(Throwable e){
+			   System.err.println("Error came while waiting for the alert popup. "+e.getMessage());
+			}
+	}
+	
+	@When("^click on enrol as RC$")
+	public void click_on_enrol_as_RC() throws Throwable {
+		online_Login_VOPgObject = new Online_Login_VOPgObject(driver);
+		online_Login_VOPgObject.clickLogRC();
+	}
+
+	@When("^check referrer \"([^\"]*)\"$")
+	public void check_referrer(String arg1) throws Throwable {
+	 
+	}
+
+	@When("^select country \"([^\"]*)\"$")
+	public void select_country(String arg1) throws Throwable {
+	  
+	}
+
+	@When("^click on shop button$")
+	public void click_on_shop_button() throws Throwable {
+	  
+	}
+
 }
--- src/test/resources/features/Vlidate_Login_Page.feature
+++ src/test/resources/features/Vlidate_Login_Page.feature
@@ -1,12 +1,65 @@
 Feature: Validate Login Page
 
+
+
 @test
-Scenario Outline: Verify if IR is able to login with correct credentials 
+Scenario Outline: VO1_1.33, VO1_1.34 Verify login using different Languages 
 Given User opens "<PLAN>" home page
 When User click on ir login
-And User enter valid "<UserName>" and "<Password>"
+And click on vo_option "<vo_option>"
+And select the language "<lang>"
+And User enter valid "<UserName>" and "<Password>" 
 Then User should able to login to the system
 
 Examples:
-|PLAN     | UserName | Password |
-| world   | AB333693 | testpw   |
+|PLAN  |vo_option|lang          |UserName|Password|
+|world |current  |ID - Indonesia|AB333693|testpw  |
+|world |current  |MY - Myanmar  |AB333693|testpw  |
+
+@test
+Scenario Outline: VO1_1.36 Verify unable to login with invalid credentials, VO1_1.50 Register only account,VO1_1.24 ransferred IR account, VO1_1.45 LOGIN / VO Blocked
+Given User opens "<PLAN>" home page
+When User click on ir login
+And click on vo_option "<vo_option>"
+And User enter valid "<UserName>" and "<Password>" 
+Then User should not be able to login to the system
+
+Examples:
+|PLAN  |vo_option|UserName|Password|
+|world |current  |HY934645|testpw  |
+|world |current  |HZ698758|testpw  |
+|world |current  |HC062000|testpw  |
+|world |current  |AB206443|testpw  |
+
+@test
+Scenario Outline: VO1_1.38  BronzeIR, VO1_1.41 Gold Star-Migrate from QNet Senior Rank, VO1_1.47 RC, VO1_1.20 Non-renewed IR 5 years, VO1_1.39 Silver IR, VO1_1.44 Diamond  IR, VO1_1.46 Change Address (AE to other WP country)
+Given User opens "<PLAN>" home page
+When User click on ir login
+And click on vo_option "<vo_option>"
+And User enter valid "<UserName>" and "<Password>" 
+Then User should able to login to the system
+
+Examples:
+|PLAN  |vo_option|UserName|Password|
+|world |current  |HD000176|testpw  |
+#|world |current  |AB206443|testpw  |
+|world |current  |CU000489|testpw  |
+|world |current  |HA148760|testpw  |
+|world |current  |HD328443|testpw  |
+|world |current  |HD328717|testpw  |
+|world |current  |HY187731|testpw  |
+
+@test
+Scenario Outline: VO1_1.49 Enrol Retail Customer under WP UAE
+Given User opens "<PLAN>" home page
+When User click on ir login
+And click on vo_option "<vo_option>"
+And click on enrol as RC 
+And check referrer "<ref_id>"
+And select country "<country>"
+And click on shop button 
+
+Examples:
+|PLAN  |vo_option|ref_id|country|
+|world |current  |||
+|world |current  |||
